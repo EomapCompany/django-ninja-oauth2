b1f7fae98ba69fe3a68fa69877ebf8240fa1e46a
Marcel <siegmann@eomap.de>
2025-09-10 10:43:13 +0200

    update version

--- ninja_oauth2/__init__.py
+++ ninja_oauth2/__init__.py
@@ -1,6 +1,6 @@
 """Django Ninja OAuth2 library"""
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from ninja_oauth2.main import NinjaAPIOAuth2
 from ninja_oauth2.openapi.docs import SwaggerOAuth2
